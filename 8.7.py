212d827fdce76ffd13f51ae081d76ee772d4d0d6
SantiGuaqueta <124614924+SantiGuaqueta@users.noreply.github.com>
2023-04-22 20:16:37 -0500

    Add files via upload

---
+++ 8.7.py
@@ -0,0 +1,41 @@
+# Ejercicio 7
+range(1,91) # Declaramos un rango 
+#Tabla de 1
+for x in range(1,11):print("1 x "+ str(x)+" = "+str(x)) # Aqui declaramos un rango del 1 al 11 pero como en todo rango el ultimo digito se le resta 1 osea aqui se hace es una lista del 1 al 10 y esta la imprimimos puesto que la tabla del 1 solo son la secuencia del 1 al 10
+
+# Tabla del 2
+for x in range(1,11): # Declaramos el primr for con el rango de la tabla 1
+    y = 2 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 2
+    print("2 x "+ str(x)+" = "+str(y)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 2 asi obtiniendo la tabla del 2
+# Tabla del 3
+for x in range(1,11):
+    z = 3 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 3
+    print("3 x "+ str(x)+" = "+str(z))  # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 3 asi obtiniendo la tabla del 3
+# Tabla del 4
+for x in range(1,11):
+    a = 4 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 4
+    print("4 x "+ str(x)+" = "+str(a)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 4 asi obtiniendo la tabla del 4
+# Tabla del 5
+for x in range(1,11):
+    b = 5 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 5
+    print("5 x "+ str(x)+" = "+str(b)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 5 asi obtiniendo la tabla del 5
+# Tabla del 6
+for x in range(1,11):
+    c = 6 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 6
+    print("6 x "+ str(x)+" = "+str(c)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 6 asi obtiniendo la tabla del 6
+# Tabla del 7
+for x in range(1,11):
+    d = 7 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 7
+    print("7 x "+ str(x)+" = "+str(d)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 7 asi obtiniendo la tabla del 7
+# Tabla del 8
+for x in range(1,11):
+    e = 8 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 8
+    print("8 x "+ str(x)+" = "+str(e)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 8 asi obtiniendo la tabla del 8
+# Tabla del 9
+for x in range(1,11):
+    f = 9 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 9
+    print("9 x "+ str(x)+" = "+str(f)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 9 asi obtiniendo la tabla del 9
+# Tabla del 10
+for x in range(1,11):
+    g = 10 * x # Declaramos una variable y la cual tiene como operacion multiplicar los valores de x con el numero 10
+    print("10 x "+ str(x)+" = "+str(g)) # Imprimimos x para que nos de la lista del 1 al 10 normal y a y la cual nos dara la misma lista pero cada valor se multiplico por 10 asi obtiniendo la tabla del 10
